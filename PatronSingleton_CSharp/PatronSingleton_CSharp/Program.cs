18dc9bed355809f9ed8246dea64c63ea1b733dc7
Onily Valera <70966644+OnilyValera@users.noreply.github.com>
2020-11-25 02:13:53 -0400

    Add files via upload

---
+++ PatronSingleton_CSharp/PatronSingleton_CSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PatronSingleton_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Singleton.Instance.mensaje);
+            Singleton.Instance.mensaje = "Hola Onily";
+            Console.WriteLine(Singleton.Instance.mensaje);
+        }
+    }
+}
---
+++ PatronSingleton_CSharp/PatronSingleton_CSharp/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PatronSingleton_CSharp
+{
+
+        public class Singleton
+        {
+            private static Singleton instance = null;
+            public string mensaje = "";
+
+            protected Singleton()
+            {
+                mensaje = "Hola Mundo";
+            }
+
+            public static Singleton Instance
+            {
+                get
+                {
+                    if (instance == null)
+                        instance = new Singleton();
+
+                    return instance;
+                }
+            }
+        }
+    
+}
